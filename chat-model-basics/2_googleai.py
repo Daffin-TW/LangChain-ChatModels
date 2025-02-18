d75de065855bb768998bbb94c4e4c7f9a0e3512b
Daffin-TW <dtwdaffintw@gmail.com>
2025-02-18 22:00:50 +0700

    Adding Google AI LLM API usage

---
+++ chat-model-basics/2_googleai.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+# Load environment variables from .env
+load_dotenv()
+
+# Declare a GPT4o mini model
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+# Send a message query and get model's result
+query = 'Who is the third president of Indonesia?'
+result = model.invoke(query)
+
+print(result.content)
