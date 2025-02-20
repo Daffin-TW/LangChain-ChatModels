2e1150eb8555c3a6fab5abadf9a6bf6d35d37ae2
Daffin-TW <dtwdaffintw@gmail.com>
2025-02-20 13:19:11 +0700

    Renamed the file

---
+++ chat-model-basics/basic_messages.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+# Load environment variables from .env
+load_dotenv()
+
+# Declare a Gemini model
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+# Send a message query and get model's result
+query = 'Who is the third president of Indonesia?'
+result = model.invoke(query)
+
+print('AI Result:')
+print(result)
+print('Output Messages:')
+print(result.content)  # Show only AI messages output
